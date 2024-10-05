53e6ab6071e75db7aa9d7d7d766df066701f0e53
maxpol <mpoluyk@gmail.com>
2024-10-05 22:14:25 +0300

    changes reverted

--- main.py
+++ main.py
@@ -18,15 +18,15 @@ if __name__ == '__main__':
             MessageHandler(filters.Regex(r"^Search LIVE game by id$"), message_handler)
         ],
         states={
-            MATCH_ID: [MessageHandler(filters.TEXT & filters.COMMAND, match_id_handler)]
+            MATCH_ID: [MessageHandler(filters.TEXT & ~filters.COMMAND, match_id_handler)]
         },
         fallbacks=[]
     )
 
     application = ApplicationBuilder().token(os.getenv('TOKEN')).build()
-    application.add_handler(conv_handler)
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("faq", faq))
+    application.add_handler(conv_handler)
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, message_handler))
     application.run_webhook(
         listen="0.0.0.0",
