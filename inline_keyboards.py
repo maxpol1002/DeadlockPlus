8a3f20313b4e1411acf5716d6b4f3413f9b95f71
maxpol <mpoluyk@gmail.com>
2024-10-15 17:47:40 +0300

    small visual change

--- inline_keyboards.py
+++ inline_keyboards.py
@@ -11,7 +11,7 @@ def create_inline_matches(matches, user_id) -> InlineKeyboardMarkup:
     for match_data in matches:
         user_hero = get_user_hero(match_data, user_uid)
         hero_icon = get_hero_icon(user_hero)
-        button_match_text.append([InlineKeyboardButton(f"{match_data['start_time']} | {hero_icon} {user_hero} | {match_data['match_elo']}",
+        button_match_text.append([InlineKeyboardButton(f"{match_data['start_time']} | {hero_icon} {user_hero} | ELO: {match_data['match_elo']}",
                                                        callback_data=f"match_{match_data['match_id']}")])
 
     return InlineKeyboardMarkup(button_match_text)
