26165e71d8877d7e87903c8b0d8e8a0db215cae0
maxpol <mpoluyk@gmail.com>
2024-10-17 19:22:50 +0300

    match mode simplified to U/R instead of Unranked/Ranked

--- inline_keyboards.py
+++ inline_keyboards.py
@@ -14,6 +14,11 @@ def create_inline_matches(matches, user_id) -> InlineKeyboardMarkup:
             user_hero = get_user_hero(match_data, user_uid)
             hero_icon = get_hero_icon(user_hero)
             if match_mode:
+                if match_mode == "Ranked":
+                    match_mode = 'R'
+                elif match_mode == "Unranked":
+                    match_mode = 'U'
+
                 button_match_text.append([InlineKeyboardButton(f"[{match_mode}] | {match_data['start_time']} "
                                                                f"| {hero_icon} {user_hero} | ELO: {match_data['match_elo']}",
                                                                callback_data=f"match_{match_data['match_id']}")])
