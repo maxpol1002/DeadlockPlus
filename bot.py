736b8282d37c34d1b6c5ea8d15e0e5879f128e1e
maxpol <mpoluyk@gmail.com>
2024-12-02 16:42:50 +0200

    better fix

--- bot.py
+++ bot.py
@@ -457,7 +457,7 @@ def create_match_stats(match_data, user_uid):
             message += "⬇️<b>Team Sapphire Flame</b>⬇️\n"
             message = "===========================\n"
 
-        player_name = player['player_name'] if player['player_name'] else "hidden name"
+        player_name = player['player_name'] if player['player_name'] else "invisible"
         message += f" - {get_hero_icon(player['hero'])} <b>{player['hero']}</b> (<a href='{player['account_link']}'><b>{player_name}</b></a>)\n"
 
         position += 1
