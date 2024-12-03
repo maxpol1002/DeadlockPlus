b2b12de386ca867a07679d9640b9038da1cb7b85
maxpol <mpoluyk@gmail.com>
2024-12-03 17:02:46 +0200

    prob fixed

--- inline_keyboards.py
+++ inline_keyboards.py
@@ -52,7 +52,7 @@ def create_inline_leaderboard(users_avg_elo: dict):
         button_user_text.append([InlineKeyboardButton(f"Username - {users_data[str(comm_id)]['username']}, ELO - {avg_elo}",
                                 callback_data=f"hz")])
 
-    return users_data
+    return InlineKeyboardMarkup(button_user_text)
 
 
 def get_elo_gain(elo_int: int) -> str:
